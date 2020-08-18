99b73e07cbb55c4e8728c6155a84f93a3e06419b
Jerud-Moyer <jerudmoyer@gmail.com>
2020-08-18 14:06:25 -0700

    here we go

---
+++ data/todos.js
@@ -0,0 +1,17 @@
+module.exports = [
+  {
+    id: 1,
+    todo: 'finish lab',
+    completed: false,
+  },
+  {
+    id: 2,
+    todo: 'drink a beer',
+    completed: false,
+  },
+  {
+    id: 3,
+    todo: 'got to sleep',
+    completed: false,
+  }
+];
